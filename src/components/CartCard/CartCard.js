6568308de628366b15411f32e8d5dcf046653c8f
albiummid <albi.ummid@gmail.com>
2021-06-07 00:08:30 +0600

    completed

--- src/components/CartCard/CartCard.js
+++ src/components/CartCard/CartCard.js
@@ -10,24 +10,20 @@ const CartCard = ({ handleQuantity, food }) => {
     const { name, price, _id, quantity, image } = food;
     return (
         <div className="cart-card-container">
-            <div className="card-catagory">
-                <div className="card-catagory-image">
-                    <img src={image} alt="" />
-                </div>
-                <div className="card-catagory-info">
-                    <p>{name}</p>
-                    <p style={{ color: "red" }}>$ {price}</p>
-                    <p>delevery fee</p>
-                </div>
+            <div className="card-catagory-image">
+                <img src={image} alt="" />
+            </div>
+            <div className="card-catagory-info">
+                <p>{name}</p>
+                <p style={{ color: "red" }}>$ {price}</p>
+                <p>delevery fee</p>
+            </div>
 
-                <div className="card-catagory-action">
+            <div className="card-catagory-action">
 
-                    <div className="quantity-div">
-                        <button onClick={() => handleQuantity("increase", _id, quantity)} ><FontAwesomeIcon icon={faPlus} /></button><h3>{quantity}</h3><button onClick={() => handleQuantity("decrease", _id, quantity)}><FontAwesomeIcon icon={faMinus} /></button> </div>
-                    <button className="delete-btn" onClick={() => dispatch(removeFromCart(food))}><img src={deleteIcon} alt="" /> </button>
-                </div>
-
-
+                <div className="quantity-div">
+                    <button onClick={() => handleQuantity("increase", _id, quantity)} ><FontAwesomeIcon icon={faPlus} /></button><h3>{quantity}</h3><button onClick={() => handleQuantity("decrease", _id, quantity)}><FontAwesomeIcon icon={faMinus} /></button> </div>
+                <button className="delete-btn" onClick={() => dispatch(removeFromCart(food))}><img src={deleteIcon} alt="" /> </button>
             </div>
 
         </div>
